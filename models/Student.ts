34438640f7d8e5ff8101be3d90df0e00ececfd11
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2024-11-25 18:46:16 +0700

    Update Student.ts

--- models/Student.ts
+++ models/Student.ts
@@ -4,6 +4,7 @@ interface IStudent extends Document {
   fristname: string;
   lastname: string;
   g_level: string;
+  grade: string;
   createdAt: Date;
 }
 
@@ -11,6 +12,7 @@ const StudentSchema = new Schema<IStudent>({
   fristname: { type: String, required: true },
   lastname: { type: String, required: true },
   g_level: { type: String },
+  grade: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
